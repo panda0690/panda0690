10f10f1825a2dbed882e1e373a62899e87647046
panda0690 <110589939+panda0690@users.noreply.github.com>
2025-04-17 20:30:49 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Custom Pizza Delivery Job for QBCore'
+author 'panda69'
+
+shared_script '@qb-core/shared/locale.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
